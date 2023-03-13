fbdb78694d1d70a36088725297fbc2638098ff9b
Juanjuan <liujuanjuan134@foxmail.com>
2023-03-13 23:54:12 +0800

    LJJ SRP 0.5/1

---
+++ LJJ/UnitySRP/Assets/My Pipeline/MyPipelineAsset.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+// Experimental:实验性namespace
+using UnityEngine.Experimental.Rendering;
+
+// 管线类，负责渲染过程。RenderPipeline继承自IRenderPipeline
+public class MyPipeline : RenderPipeline
+{
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+
+        //{
+        //    // 发送渲染命令
+        //    renderContext.DrawSkybox(cameras[0]);
+
+        //    // 对上下文的命令是缓冲的，提交后执行
+        //    renderContext.Submit();
+        //}
+
+        // 逐相机渲染
+        foreach (var camera in cameras)
+        {
+            Render(renderContext, camera);
+        }
+    }
+
+    public void Render(ScriptableRenderContext renderContext, Camera camera)
+    {
+        // 设置摄像机视图矩阵V和投影矩阵P等
+        renderContext.SetupCameraProperties(camera);
+
+        // 仅用到相机清除标志决定是否绘制天空盒
+        renderContext.DrawSkybox(camera);
+
+        renderContext.Submit();
+    }
+}
+
+// 管线资产的描述
+[CreateAssetMenu(menuName = "Rendering/My Pipeline")]
+public class MyPipelineAsset : RenderPipelineAsset
+{
+    // 创建管线实例
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new MyPipeline();
+    }
+}
---
+++ LJJ/UnitySRP/UnitySRP.md
@@ -0,0 +1,17 @@
+https://zhuanlan.zhihu.com/p/165140042
+
+# 一
+
+问题
+
+1. 正向渲染、延迟渲染
+2. Unity2018引入了轻量级管线和高清管线，区别是什么，分别可以定制什么阶段
+3. Unity2018默认gamma空间，gamma和Linear的区别
+4. C# 成员函数的public override前缀
+
+Opaque 不透明
+
+点
+
+- Unity中可以做到通过脚本继承资产类型来实现定制（RenderPipelineAsset）
+- 
