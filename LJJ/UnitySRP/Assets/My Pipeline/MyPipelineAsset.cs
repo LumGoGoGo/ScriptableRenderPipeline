ac0e5720ee1d39f108b548a08ddd8b2e6dea500c
Juanjuan <liujuanjuan134@foxmail.com>
2023-03-16 10:18:49 +0800

    临时分支提交未完成的作业1

--- LJJ/UnitySRP/Assets/My Pipeline/MyPipelineAsset.cs
+++ LJJ/UnitySRP/Assets/My Pipeline/MyPipelineAsset.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
 // Experimental:实验性namespace
 using UnityEngine.Experimental.Rendering;
 
@@ -24,13 +25,60 @@ public class MyPipeline : RenderPipeline
         }
     }
 
+    CullResults cull;
+    CommandBuffer cameraBuffer = new CommandBuffer { name = "Render Camara" };
+
     public void Render(ScriptableRenderContext renderContext, Camera camera)
     {
         // 设置摄像机视图矩阵V和投影矩阵P等
         renderContext.SetupCameraProperties(camera);
 
-        // 仅用到相机清除标志决定是否绘制天空盒
-        renderContext.DrawSkybox(camera);
+        {
+            // 命令缓冲区
+            //var buffer = new CommandBuffer { name = camera.name };
+
+            // 根据camera属性，添加clear命令
+            CameraClearFlags clearFlags = camera.clearFlags;
+            cameraBuffer.ClearRenderTarget(
+                (clearFlags & CameraClearFlags.Depth) != 0,
+                (clearFlags & CameraClearFlags.Color) != 0,
+                camera.backgroundColor);
+
+            // 复制到上下文内部缓冲区
+            renderContext.ExecuteCommandBuffer(cameraBuffer);
+            //buffer.Release();
+            cameraBuffer.Clear();
+        }
+
+        {
+            // 获取剔除参数
+            ScriptableCullingParameters cullingParameters;
+            if (CullResults.GetCullingParameters(camera, out cullingParameters) == false)
+            {
+                // 创建有效参数失败
+                return;
+            }
+
+            // 剔除
+            CullResults.Cull(ref cullingParameters, renderContext, ref cull);
+
+            // 绘制不透明物体。先绘制不透明的，防止overdraw
+            var drawSettings = new DrawRendererSettings(camera, new ShaderPassName("SRPDefaultUnlit"));
+            // 不透明从前往后绘制，减少overdraw
+            drawSettings.sorting.flags = SortFlags.CommonOpaque;
+            var filterSettings = new FilterRenderersSettings(true) { renderQueueRange = RenderQueueRange.opaque };
+            renderContext.DrawRenderers(cull.visibleRenderers, ref drawSettings, filterSettings);
+
+            // 绘制天空盒
+            // 仅用到相机清除标志决定是否绘制天空盒
+            renderContext.DrawSkybox(camera);
+
+            // 绘制透明物体。透明着色器通道不写入深度缓冲区，后绘制
+            drawSettings.sorting.flags = SortFlags.CommonTransparent;
+            // 透明物体从后往前绘制，透明效果需要叠加之前的颜色
+            filterSettings.renderQueueRange = RenderQueueRange.transparent;
+            renderContext.DrawRenderers(cull.visibleRenderers, ref drawSettings, filterSettings);
+        }
 
         renderContext.Submit();
     }
--- LJJ/UnitySRP/UnitySRP.md
+++ LJJ/UnitySRP/UnitySRP.md
@@ -8,10 +8,16 @@ https://zhuanlan.zhihu.com/p/165140042
 2. Unity2018引入了轻量级管线和高清管线，区别是什么，分别可以定制什么阶段
 3. Unity2018默认gamma空间，gamma和Linear的区别
 4. C# 成员函数的public override前缀
+5. "命令缓冲区要求资源以将其命令存储在Unity引擎的本机级别。"
 
 Opaque 不透明
 
 点
 
 - Unity中可以做到通过脚本继承资产类型来实现定制（RenderPipelineAsset）
-- 
+- C# 的out、ref参数
+- RenderQueueRange.opaque覆盖从0到2500，RenderQueueRange.transparent从2501到5000
+
+说明
+
+- 用FilterRenderersSettings而不用FilterRendererSettings，可能因为写教程时没有不带s的，用2018.3.0f2没有不带s的
